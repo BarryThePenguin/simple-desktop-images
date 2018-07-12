e3a805b01ea78a36cc18f3e872af3a2d43066792
Jonathan Haines <jonno.haines@gmail.com>
2018-07-12 16:17:59 +1000

    update package-lock.json

--- flow-typed/npm/rxjs_v6.x.x.js
+++ flow-typed/npm/rxjs_v6.x.x.js
@@ -1,5 +1,5 @@
-// flow-typed signature: 7dca9ec48b178e2f0296caf32b123aa0
-// flow-typed version: 71a8210c07/rxjs_v6.x.x/flow_>=v0.34.x
+// flow-typed signature: 78102c9378223bdd9a7442cccf401be8
+// flow-typed version: 3836987de0/rxjs_v6.x.x/flow_>=v0.34.x
 
 type rxjs$PartialObserver<-T> =
   | {
@@ -1638,6 +1638,82 @@ declare module "rxjs" {
       input: rxjs$ObservableInput<T>,
       scheduler?: rxjs$SchedulerClass
     ): rxjs$Observable<T>,
+    empty<+T>(): rxjs$Observable<T>,
+    bindNodeCallback<U>(
+      callbackFunc: (callback: (err: any, result: U) => any) => any,
+      selector?: void,
+      scheduler?: rxjs$SchedulerClass
+    ): () => rxjs$Observable<U>,
+    bindNodeCallback<T, U>(
+      callbackFunc: (v1: T, callback: (err: any, result: U) => any) => any,
+      selector?: void,
+      scheduler?: rxjs$SchedulerClass
+    ): (v1: T) => rxjs$Observable<U>,
+    bindNodeCallback<T, T2, U>(
+      callbackFunc: (
+        v1: T,
+        v2: T2,
+        callback: (err: any, result: U) => any
+      ) => any,
+      selector?: void,
+      scheduler?: rxjs$SchedulerClass
+    ): (v1: T, v2: T2) => rxjs$Observable<U>,
+    bindNodeCallback<T, T2, T3, U>(
+      callbackFunc: (
+        v1: T,
+        v2: T2,
+        v3: T3,
+        callback: (err: any, result: U) => any
+      ) => any,
+      selector?: void,
+      scheduler?: rxjs$SchedulerClass
+    ): (v1: T, v2: T2, v3: T3) => rxjs$Observable<U>,
+    bindNodeCallback<T, T2, T3, T4, U>(
+      callbackFunc: (
+        v1: T,
+        v2: T2,
+        v3: T3,
+        v4: T4,
+        callback: (err: any, result: U) => any
+      ) => any,
+      selector?: void,
+      scheduler?: rxjs$SchedulerClass
+    ): (v1: T, v2: T2, v3: T3, v4: T4) => rxjs$Observable<U>,
+    bindNodeCallback<T, T2, T3, T4, T5, U>(
+      callbackFunc: (
+        v1: T,
+        v2: T2,
+        v3: T3,
+        v4: T4,
+        v5: T5,
+        callback: (err: any, result: U) => any
+      ) => any,
+      selector?: void,
+      scheduler?: rxjs$SchedulerClass
+    ): (v1: T, v2: T2, v3: T3, v4: T4, v5: T5) => rxjs$Observable<U>,
+    bindNodeCallback<T, T2, T3, T4, T5, T6, U>(
+      callbackFunc: (
+        v1: T,
+        v2: T2,
+        v3: T3,
+        v4: T4,
+        v5: T5,
+        v6: T6,
+        callback: (err: any, result: U) => any
+      ) => any,
+      selector?: void,
+      scheduler?: rxjs$SchedulerClass
+    ): (v1: T, v2: T2, v3: T3, v4: T4, v5: T5, v6: T6) => rxjs$Observable<U>,
+    bindNodeCallback<T>(
+      callbackFunc: Function,
+      selector?: void,
+      scheduler?: rxjs$SchedulerClass
+    ): (...args: Array<any>) => rxjs$Observable<T>,
+    bindNodeCallback<T>(
+      callbackFunc: Function,
+      selector?: (...args: Array<any>) => T,
+      scheduler?: rxjs$SchedulerClass
+    ): (...args: Array<any>) => rxjs$Observable<T>,
     timer(
       initialDelay: number | Date,
       period?: number,
